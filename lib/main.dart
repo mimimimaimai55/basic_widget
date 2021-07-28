01c8538d344bc770a13f4685951c75dbfefb0b25
mizuhomai <choco.hana.choco@gmail.com>
2021-07-28 16:44:43 +0900

    練習

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:basic_widget/next_page.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -61,6 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
             },
           ),
       ),
+
 
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
